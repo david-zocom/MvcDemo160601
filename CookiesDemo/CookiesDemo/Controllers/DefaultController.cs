d9dde2ca5c7064491bdc87304521ab24517dbe0a
David Andersson <david.andersson@alumni.chalmers.se>
2016-06-01 10:55:51 +0200

    initial

---
+++ CookiesDemo/CookiesDemo/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookiesDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            HttpCookie cookie = Request.Cookies["cookie"];
+
+            return View();
+        }
+    }
+}
