c675a8acc728a23226e7b745b277d4b2e8a57004
David Andersson <david.andersson@alumni.chalmers.se>
2016-06-01 13:16:17 +0200

    final version

--- CookiesDemo/CookiesDemo/Scripts/JavaScript.js
+++ CookiesDemo/CookiesDemo/Scripts/JavaScript.js
@@ -8,10 +8,15 @@
     }); //btnIncrease on click
 
     $('#btnDecrease').on('click', function () {
-        var el = $('#counter');
+        var el = document.getElementById('counter');
+        var x = el.dataset.david - 0;
+        x = x - 1;
+        el.dataset.david = x;
+        el.innerHTML = x;
+        /*var el = $('#counter');
         var x = el.data('david') - 0;
         x = x - 1;
         el.data('david', x);
-        el.html(x);
+        el.html(x);*/
     });
 }); // document ready
