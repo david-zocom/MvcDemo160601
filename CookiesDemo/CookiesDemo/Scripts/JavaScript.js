05c345d86b41e956d9f268f6ec39d5679543e25e
David Andersson <david.andersson@alumni.chalmers.se>
2016-06-01 13:01:23 +0200

    data attribute

---
+++ CookiesDemo/CookiesDemo/Scripts/JavaScript.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#btnIncrease').on("click", function () {
+        var el = document.getElementById('counter');
+        var x = el.dataset.value - 0;
+        x = x + 1;
+        el.dataset.value = x;
+        el.innerHTML = x;
+    }); //btnIncrease on click
+
+
+}); // document ready
